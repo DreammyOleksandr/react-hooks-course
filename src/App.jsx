d6246807f8759b7e208fb16d1049a3b72de53fc9
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-09-20 10:43:09 +0300

    upd: useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [value, setValue] = useState('Alex')
+  let onChange = (event) => setValue(event.target.value)
+  
   return (
-    <div className="App">
-       Hello World!
+    <div className='App'>
+      <input type='text' placeholder='Enter your value' onChange={onChange} />
+      <h1>{value}</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
